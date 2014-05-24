f14d38ecddd37a7e282418c65caeec124faa5ebe
Ulf Johanneson <uffe.johanneson@gmail.com>
2014-05-24 23:12:16 +0200

    Add PBE encrypted file support to Storage

--- safestore/src/main/java/se/cenote/safestore/domain/Storage.java
+++ safestore/src/main/java/se/cenote/safestore/domain/Storage.java
@@ -1,17 +1,20 @@
 package se.cenote.safestore.domain;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import se.cenote.safestore.domain.crypto.CryptoManager;
 
 public class Storage {
+	
+	private char[] pwd;
+	private CryptoManager crypto;
+	
+	public Storage(){
+		crypto = new CryptoManager();
+		pwd = "abc123".toCharArray();
+	}
 	
 	private File getFile(){
 		File file = new File(System.getProperty("user.home"), ".safeStore");
@@ -22,66 +25,38 @@ public class Storage {
 		
 		File file = new File(System.getProperty("user.home"), ".safeStore");
 		
-		BufferedWriter writer = null;
-		try{
-			writer = new BufferedWriter(new FileWriter(file));
-			for(Entry entry : entries){
-				writer.write(entry.getName() + ", ");
-				writer.write(entry.getUsername() + ", ");
-				
-				//String base64 = asBase64(entry.getPwd());
-				String base64 = CryptoManager.encrypt(entry.getPwd());
-				
-				writer.write(base64 + "\n");
-			}
+		StringBuilder buffer = new StringBuilder();
+		for(Entry entry : entries){
+			buffer.append(entry.getName());
+			buffer.append("\n");
+			buffer.append(entry.getUsername());
+			buffer.append("\n");
+			buffer.append(new String(entry.getPwd()));
+			buffer.append("\n");
 		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		finally{
-			try {
-				writer.close();
-			} 
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		buffer.replace(buffer.length()-1, buffer.length(), "");
+		
+		crypto.storeSecure(buffer.toString(), file, pwd);
 	}
 	
 	public List<Entry> load(){
+		
 		List<Entry> list = new ArrayList<Entry>();
 		
 		File file = getFile();
 		if(file.exists()){
 			
-			BufferedReader reader = null;
-			try{
-				reader = new BufferedReader(new FileReader(file));
-				String row = null;
-				while((row = reader.readLine()) != null){
-					String[] parts = row.split(",");
-					String name = parts[0].trim();
-					String user = parts[1].trim();
-					String data = parts[2].trim();
-					
-					//byte[] pwd = asBytes(data);
-					byte[] pwd = CryptoManager.decrypt(data);
-					
-					Entry entry = new Entry(name, user, pwd);
-					list.add(entry);
-				}
-			}
-			catch(Exception e){
-				e.printStackTrace();
-			}
-			finally{
-				try {
-					if(reader != null)
-						reader.close();
-				} 
-				catch (IOException e) {
-					e.printStackTrace();
-				}
+			String data = crypto.readSecure(file, pwd);
+			
+			String[] parts = data.split("\n");
+			int i = 0;
+			while(i < parts.length){
+				String name = parts[i++];
+				String username = parts[i++];
+				String password = parts[i++];
+				
+				Entry entry = new Entry(name, username, password.getBytes());
+				list.add(entry);
 			}
 		}
 		
--- safestore/src/main/java/se/cenote/safestore/domain/crypto/CryptoManager.java
+++ safestore/src/main/java/se/cenote/safestore/domain/crypto/CryptoManager.java
@@ -1,100 +1,186 @@
 package se.cenote.safestore.domain.crypto;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.GeneralSecurityException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.security.AlgorithmParameters;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.PBEParameterSpec;
-
+import javax.crypto.spec.SecretKeySpec;
 
 public class CryptoManager {
 	
-	private static final String UTF8 = "UTF-8";
+	private byte[] salt;
+	private byte[] iv;
+
+	public static void main(String[] args) {
+		
+		String text = "test1\nuffe\nabc123\ntest2\nuffe\nabc321";
+		char[] pwd = "123".toCharArray();
+		
+		System.out.println("Text: " + text + ", pwd: " + new String(pwd));
+		
+		CryptoManager cryptoManager = new CryptoManager();
+		
+		EncryptedData data = cryptoManager.encrypt(text, pwd);
+		System.out.println("Encrypted[1]: " + data.getEncryptedBase64());
+		
+		File file = new File("batman.cry");
+		writeToFile(file, data);
+		
+		EncryptedData data2 = readFromFile(file);
+		System.out.println("Encrypted[2]: " + data2.getEncryptedBase64());
+		
+		String text2 = cryptoManager.decrypt(data2.getEncryptedBytes(), data2.getIv(), pwd);
+		
+		System.out.println(">>" + text2);
+	}
 	
-	private static final String CRYPTO = "PBEWithMD5AndDES";
+	private void test(){
+		
+		String text = "test1\nuffe\nabc123\ntest2\nuffe\nabc321";
+		char[] pwd = "123".toCharArray();
+		
+		File file = new File("batman.cry");
+		
+		storeSecure(text, file, pwd);
+		
+		String encrypted = readSecure(file, pwd);
+	}
 	
-	private static final char[] PASSWORD = "enfldsgbnlsngdlksdsgm".toCharArray();
+	public void storeSecure(String text, File file, char[] pwd){
+		CryptoManager cryptoManager = new CryptoManager();
+		EncryptedData data = cryptoManager.encrypt(text, pwd);
+		writeToFile(file, data);
+	}
 	
-    private static final byte[] SALT = {
-        (byte) 0xde, (byte) 0x33, (byte) 0x10, (byte) 0x12,
-        (byte) 0xde, (byte) 0x33, (byte) 0x10, (byte) 0x12,
-    };
+	public String readSecure(File file, char[] pwd){
+		EncryptedData data = readFromFile(file);
+		
+		CryptoManager cryptoManager = new CryptoManager();
+		return cryptoManager.decrypt(data.getEncryptedBytes(), data.getIv(), pwd);
+	}
+	
+	public static EncryptedData readFromFile(File file) {
+		EncryptedData data = null;
+		
+		try{
+			Path path = Paths.get(file.getAbsolutePath());
+			byte[] arr = Files.readAllBytes(path);
+			byte[] iv = Arrays.copyOfRange(arr, 0, 16);
+			byte[] encryptedBytes = Arrays.copyOfRange(arr, 16, arr.length);
+			data = new EncryptedData(encryptedBytes, iv);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return data;
+	}
 
-    public static void main(String[] args) throws Exception {
-        
-    	String originalPassword = "secret";
-    	
-        System.out.println("Original password: " + originalPassword);
-        
-        String encryptedPassword = encrypt(originalPassword);
-        System.out.println("Encrypted password: " + encryptedPassword);
-        
-        String decryptedPassword = decryptToString(encryptedPassword);
-        System.out.println("Decrypted password: " + decryptedPassword);
-    }
-    
-    public static String encrypt(String text) throws GeneralSecurityException, UnsupportedEncodingException {
-        return encrypt(text.getBytes(UTF8));
-    }
+	public static void writeToFile(File file, EncryptedData data) {
+		
+	    try {
+	    	Path path = Paths.get(file.getAbsolutePath());
+	    	Files.write(path, data.getIv());
+	    	Files.write(path, data.getEncryptedBytes(), StandardOpenOption.APPEND);
+	    }
+	    catch(Exception e){
+	    	e.printStackTrace();
+	    }
+	}
 
-    public static String encrypt(byte[] bytes) throws GeneralSecurityException, UnsupportedEncodingException {
-        Cipher cipher = getCipher(Cipher.ENCRYPT_MODE);
-        return base64Encode(cipher.doFinal(bytes));
-    }
-    
-    public static String decryptToString(String data) throws GeneralSecurityException, IOException {
-        return new String(decrypt(data), UTF8);
-    }
+	public CryptoManager(){
+		salt = new byte[8];
+		new SecureRandom().nextBytes(salt);
+	}
+	
+	public EncryptedData encrypt(String text, char[] pwd){
+		EncryptedData data = null;
+		
+		try{
+			
+			SecretKey secretKey = generateSecretKey(pwd, salt);
+			
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+			AlgorithmParameters params = cipher.getParameters();
+			iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+			byte[] encryptedData = cipher.doFinal(text.getBytes("UTF-8"));
+			
+			data = new EncryptedData(encryptedData, iv);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return data;
+	}
+	
+	public String decrypt(byte[] encryptedText, byte[] iv, char[] pwd){
+		
+		byte[] decrypted = null;
+		
+		try{
+			SecretKey secretKey = generateSecretKey(pwd, salt);
+			
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
+			decrypted = cipher.doFinal(encryptedText);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return new String(decrypted);
+	}
+	
+	
+	private static SecretKey generateSecretKey(char[] pwd, byte[] salt){
+		SecretKey secretKey = null;
+		
+		try{
+			SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+			KeySpec spec = new PBEKeySpec(pwd, salt, 65536, 256);
+			SecretKey tmp = factory.generateSecret(spec);
+			
+			secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return secretKey;
+	}
 
-    public static byte[] decrypt(String data) throws GeneralSecurityException, IOException {
-    	Cipher cipher = getCipher(Cipher.DECRYPT_MODE);
-        return cipher.doFinal(base64Decode(data));
-    }
-    
-    private static Cipher getCipher(int mode) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException, InvalidKeyException, InvalidAlgorithmParameterException{
-    	SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(CRYPTO);
-    	SecretKey key = keyFactory.generateSecret(new PBEKeySpec(PASSWORD));
-        Cipher cipher = Cipher.getInstance(CRYPTO);
-        cipher.init(mode, key, new PBEParameterSpec(SALT, 20));
-        return cipher;
-    }
-    
-    /*
-    private static String getCipher(){
-    	SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-    	KeySpec spec = new PBEKeySpec(PASSWORD, SALT, 65536, 256);
-    	SecretKey tmp = factory.generateSecret(spec);
-    	SecretKey secret = new SecretKeySpec(tmp.getEncoded(), "AES");
-    	
-    	// Encrypt the message.
-    	Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-    	cipher.init(Cipher.ENCRYPT_MODE, secret);
-    	AlgorithmParameters params = cipher.getParameters();
-    	byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
-    	byte[] ciphertext = cipher.doFinal("Hello, World!".getBytes(UTF8));
-    	
-    	// Decrypt the message, given derived key and initialization vector.
-    	Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-    	cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(iv));
-    	String plaintext = new String(cipher.doFinal(ciphertext), UTF8);
-    	System.out.println(plaintext);
-    }
-    */
+	public static class EncryptedData{
+		
+		private byte[] encryptedBytes;
+		private byte[] iv;
+		
+		private String encryptedBase64;
+		
+		public EncryptedData(byte[] encryptedBytes, byte[] iv) {
+			this.encryptedBytes = encryptedBytes;
+			this.iv = iv;
+			
+			encryptedBase64 = Base64.getEncoder().encodeToString(encryptedBytes);
+		}
+		public byte[] getEncryptedBytes() {
+			return encryptedBytes;
+		}
+		public byte[] getIv() {
+			return iv;
+		}
+		public String getEncryptedBase64(){
+			return encryptedBase64;
+		}
+	}
 
-    private static String base64Encode(byte[] bytes) {
-    	return Base64.getEncoder().encodeToString(bytes);
-    }
-
-    private static byte[] base64Decode(String data) throws IOException {
-        return Base64.getDecoder().decode(data);
-    }
 }
